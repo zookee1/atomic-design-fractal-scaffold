19b4cacd13e60f911118053e6ec30fde53c8321e
Dennis Heibült <dennis@csshugs.com>
2017-07-25 14:29:13 +0200

    Rename demo pattern

---
+++ components/01-atoms/10-buttons/buttons.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "default": "Button Primary",
+    "context": {
+        "button-modifier": "c-btn--primary"
+    },
+    "variants": [
+        {
+            "name": "Button Secondary",
+            "context": {
+                "button-modifier": "c-btn--secondary"
+            }
+        },
+        {
+            "name": "Button Tertiary",
+            "context": {
+                "button-modifier": "c-btn--tertiary"
+            }
+        },
+        {
+            "name": "Button Small",
+            "context": {
+                "button-modifier": "c-btn--primary c-btn--small"
+            }
+        },
+        {
+            "name": "Button Large",
+            "context": {
+                "button-modifier": "c-btn--primary c-btn--large"
+            }
+        }
+    ]
+}
